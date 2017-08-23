98bb16dce356cd0208914383f2aa98fe84e1e793
Stanford Rosenthal <nolastan@gmail.com>
2017-08-23 16:49:18 -0500

    use mocked docket data, some styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import JudgeCard from './cards/JudgeCard.js';
 import CounselCard from './cards/CounselCard.js';
-import './App.css';
 
+// Stubbed Data
 import JudgeData from './data/judges.json';
-import CounselData from './data/counsel.json';
+import CounselData from './data/counsels.json';
+
+var docket = {
+  "name": "Marlin Gusman",
+  "counsel": "Lucy Hall",
+  "section": "E",
+}
 
 class App extends Component {
 
   render() {
-    var judge = JudgeData.judges.A;
-
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Pretrial App</h2>
-        </div>
-        <p className="App-intro">
-        Jason, your arraignment is in 3 days.
+        <p>
+        {docket.name}, your arraignment is in 3 days.
         </p>
-        <JudgeCard judge={JudgeData.judges.A} />
-        <CounselCard counsel={CounselData.counsels[0]} />
+        <JudgeCard judge={JudgeData.data[docket.section]} />
+        <CounselCard counsel={CounselData.data[docket.counsel]} />
       </div>
     );
   }
--- src/cards/Card.js
+++ src/cards/Card.js
@@ -4,14 +4,15 @@ const Card = styled.a`
   display: block;
   padding: 1em;
   margin: 1ex 1em;
-  background: #FEFEFE;
+  background: #FDFDFD;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
   border-radius: 2px;
   transition: 0.1s box-shadow ease-in;
+  color: #333333;
 
   &:hover {
     background: white;
-    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.4);
+    box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.5);
   }
 `
 
