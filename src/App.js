e443fdf07ea77dc6022623ebb744de91a2307f4e
Stanford Rosenthal <nolastan@gmail.com>
2017-08-29 22:50:16 -0500

    use remote data

--- src/App.js
+++ src/App.js
@@ -9,17 +9,33 @@ import JudgeData from './data/judges.json';
 import CounselData from './data/counsels.json';
 
 var docket = {
-  "name": "Marlin Gusman",
-  "counsel": "Brian Woods",
-  "section": "D",
+  "name": "Gregory Hunter",
+  "counsel": "Brian Woods"
 }
 
 var counsel = CounselData.data[docket.counsel]
-var judge = JudgeData.data[docket.section]
 
 class App extends Component {
 
+  state = {record: {}}
+
+  loadDocketByNumber(docketNumber) {
+    fetch(`http://homewardnola.herokuapp.com/api/dockets/d${docketNumber}`)
+      .then(res => res.json())
+      .then(record => this.setState({record: record}));
+  }
+
+  componentDidMount() {
+    this.loadDocketByNumber("526290")
+  }
+
   render() {
+    if(!this.state.record.docket) {
+      return (<div>Loading…</div>)
+    }
+
+    var judge = JudgeData.data[this.state.record.docket.section]
+
     return (
       <div className="App">
         <Hero name={docket.name} />
@@ -33,7 +49,7 @@ class App extends Component {
         </Card>
 
         <Card title="Your courtroom" banner="images/court.jpg">
-          <p>Orleans Parish Criminal District Court, Section {docket.section}</p>
+          <p>Orleans Parish Criminal District Court, Section {this.state.record.docket.section}</p>
           <p>Courtroom #{judge.courtroom} ({Ordinal(judge.floor)} floor)<br />
              2700 Tulane Avenue
           </p>
